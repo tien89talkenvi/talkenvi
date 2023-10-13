1bce41d764260de2c15055c862453e635357b3e0
tien89talkenvi <147873351+tien89talkenvi@users.noreply.github.com>
2023-10-14 03:45:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import speech_recognition as sr #cho speech to txt
-from googletrans import Translator #(phai update bang pip install googletrans-py )
+from googletrans-py import Translator #(phai update bang pip install googletrans-py )
 from gtts import gTTS   #cho txt to speech
 from io import BytesIO  #cho txt to speech
 from IPython.display import Audio   #cho txt to speech
