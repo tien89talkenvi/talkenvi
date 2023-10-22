da4da7f4db49f7b76bfb668ac659856f0697aec0
tien89talkenvi <147873351+tien89talkenvi@users.noreply.github.com>
2023-10-23 01:59:12 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,7 @@ from googletrans import Translator
 from gtts import gTTS, gTTSError   
 from io import BytesIO  
 import time
-
-import pybase64
+import base64
 
 def thuam_txt_dichtxt_phatam(audio_bytes,lang_sp,lang_src,lang_dest):
     with open('thu.wav','wb') as f:
@@ -40,7 +39,7 @@ def thuam_txt_dichtxt_phatam(audio_bytes,lang_sp,lang_src,lang_dest):
                 audio_placeholder = st.empty()
                 contents = mp3_fp.read()
                 mp3_fp.close()
-                audio_str = "data:audio/ogg;pybase64,%s"%(pybase64.standard_b64decode(contents).decode())
+                audio_str = "data:audio/ogg;base64,%s"%(base64.b64encode(contents).decode())
                 audio_html = """
                                 <audio autoplay=True>
                                 <source src="%s" type="audio/ogg" autoplay=True>
