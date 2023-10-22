83427d86df7e9431d5ac60159d6e1da3579fb585
tien89talkenvi <147873351+tien89talkenvi@users.noreply.github.com>
2023-10-23 01:25:45 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
+#streamlit==1.27.2
+#SpeechRecognition==3.10.0
+#pyaudio==0.2.13 #(cho SpeechRecognition lay micro )
+#googletrans==4.0.0rc1 #(phien ban nay cho rieng py khi su dung googletrans, cac pban khac hay gay loi)
+#gTTS==2.4.0
+
+
 #https://talkenvi-b5vypm7itcecxnkuvne7h9.streamlit.app/ 
 #la url app moi talkenvi
 import streamlit as st
@@ -6,87 +13,87 @@ from audio_recorder_streamlit import audio_recorder #pip install audio-recorder-
 from googletrans import Translator 
 from gtts import gTTS, gTTSError   
 from io import BytesIO  
-import soundfile as sf
-import sounddevice as sd
+import time
+import base64
 
-def speech_text_translate_audio(lang_sp,lang_src,lang_dest):
-    if lang_sp == 'vi':
-        mtextX="**A** Nói tiếng Việt (Say in Vietnamese):"
-        mrecthu="#FFFF00"
-        mrecstop="#FF0000"
-    else:
-        mtextX='**B** Nói tiếng '+noi_voi+' (Say in '+noi_voi+'):'
-        mrecthu="#FFFF00"
-        mrecstop="#0000FF"
 
-    audio_bytes = audio_recorder(text=mtextX,recording_color=mrecthu,neutral_color=mrecstop,icon_size="2x",energy_threshold=(-1.0,1.0),pause_threshold=5.0)
-    if audio_bytes:
-        with open('thu.wav','wb') as f:
-            f.write(audio_bytes)
-            r = sr.Recognizer()
-            with sr.AudioFile('thu.wav') as source:
-                audio = r.record(source)  # read the entire audio file
-            try:
-                text_from_audio = r.recognize_google(audio, language=lang_sp)
-                st.write(text_from_audio)
+def thuam_txt_dichtxt_phatam(audio_bytes,lang_sp,lang_src,lang_dest):
+    with open('thu.wav','wb') as f:
+        f.write(audio_bytes)
+        r = sr.Recognizer()
+        with sr.AudioFile('thu.wav') as source:
+            audio = r.record(source)  # read the entire audio file
+        try:
+            text_from_audio = r.recognize_google(audio, language=lang_sp)
+            st.write(text_from_audio)
+            if text_from_audio != '':
                 translator = Translator()
                 text_translated = translator.translate(text_from_audio, src=lang_src,dest=lang_dest).text    # Dich ra En theo tai lieu web
                 st.write(text_translated)
+            if text_translated !='':
                 mp3_fp = BytesIO()
                 tts = gTTS(text_translated, lang=lang_dest)
                 tts.write_to_fp(mp3_fp)
                 mp3_fp.seek(0)
                 st.audio(mp3_fp, format="audio/wav",start_time=0)
-                # Đọc dữ liệu âm thanh từ `BytesIO` bằng thư viện soundfile
-                audio_data, sample_rate = sf.read(mp3_fp)
-                # Phát âm thanh bằng thư viện sounddevice
-                sd.play(audio_data, sample_rate)
-                sd.wait()
-
-            except sr.UnknownValueError:
-                text1=''
-                st.write("")
-            except sr.RequestError as e:
-                text1=''
-                print(f"Lỗi: {e}")
-                st.write("")
-
+                # tu 1 trang app web cu a nguoi Hoa toi lay va phat thanh cong
+                audio_placeholder = st.empty()
+                contents = mp3_fp.read()
+                mp3_fp.close()
+                audio_str = "data:audio/ogg;base64,%s"%(base64.b64encode(contents).decode())
+                audio_html = """
+                                <audio autoplay=True>
+                                <source src="%s" type="audio/ogg" autoplay=True>
+                                Your browser does not support the audio element.
+                                </audio>
+                            """ %audio_str
+                audio_placeholder.empty()
+                time.sleep(0.2) 
+                audio_placeholder.markdown(audio_html, unsafe_allow_html=True)
+                # Cam on ng Hoa
+        except sr.UnknownValueError:
+            st.write("Không nhận thức được tiếng nói")
+        except sr.RequestError as e:
+            print(f"Lỗi: {e}")
 
 #######################################################
-st.subheader(":blue[Trò chuyện tiếng Việt (có thông dịch) với...]")
-#vaichon = st.radio(":green[Select one of options to say:]", 
-#                [":orange[Vietnamse]", ":blue[English]",":green[Danish]",":orange[German]",":yellow[Taiwan]",":blue[Japanese]",":red[Korean]","CANCEL"],
-#                index=7,horizontal=True ) 
-noi_voi = st.selectbox("Chon nguoi Noi voi:", 
-                ("English - Anh (en)","Spanish - Tây ban nha (es)","Taiwan - Đài loan (zh-TW)","Danish - Đan mạch (da)","German - Đức (de)","Dutch - Hà lan (nl)","Japanese - Nhật bản (ja)","Korean - Hản quốc (ko)"),index=0,label_visibility="hidden")
-noi_voi=noi_voi.strip()
+st.subheader(":blue[Trò chuyện có thông dịch bằng tiếng Việt và tiếng...]")
+tieng_khac = st.selectbox(":blue[(Talk with interpretation in Vietnamese and ...)]", 
+                ("English - Anh (en)","Spanish - Tây ban nha (es)","Taiwan - Đài loan (zh-TW)","Danish - Đan mạch (da)","German - Đức (de)","Dutch - Hà lan (nl)","Japanese - Nhật bản (ja)","Korean - Hản quốc (ko)"),index=0)
 sub1='('
 sub2=')'
-idx1 = noi_voi.index(sub1)
-idx2 = noi_voi.index(sub2)
+idx1 = tieng_khac.index(sub1)
+idx2 = tieng_khac.index(sub2)
 res = ''
 # getting elements in between
 for idx in range(idx1 + len(sub1), idx2):
-    res = res + noi_voi[idx]
+    res = res + tieng_khac[idx]
 codelang=res
 #print(codelang)
-####
-vaichon = st.radio(":green[Select one of options to say:]", 
-                [":red[Vietnamse]", ":blue["+noi_voi+"]","Clear (Xóa)"],
-                index=2,horizontal=True ) 
+
+mtext1="Click on mic để nói tiếng VIỆT (Vietnamese):"
+mtext2='Click on mic để nói tiếng '+tieng_khac
+
+vaichon = st.radio(":green[Select one of options to say:]", [":red[Nói tiếng VIỆT (Vietnamese)]",":blue[Nói tiếng "+tieng_khac+"]",'CLEAR'],index=2,horizontal=True ) 
+
 st.write("---")
 
-if vaichon=="Clear (Xóa)":
-    st.write("")
+if vaichon==":red[Nói tiếng VIỆT (Vietnamese)]":
+    audio_bytes1 = audio_recorder(text=mtext1,recording_color="#FFFF00",neutral_color="#FF0000",icon_size="2x",energy_threshold=(-1.0,1.0),pause_threshold=3.0)
+    lang_sp='vi'
+    lang_src='vi'
+    lang_dest=codelang
+    if audio_bytes1: #neu co du lieu trong bien audio_bytes2
+        thuam_txt_dichtxt_phatam(audio_bytes1,lang_sp,lang_src,lang_dest)
+
+elif vaichon==":blue[Nói tiếng "+tieng_khac+"]":
+    audio_bytes2 = audio_recorder(text=mtext2,recording_color="#FFFF00",neutral_color="#0000FF",icon_size="2x",energy_threshold=(-1.0,1.0),pause_threshold=3.0)
+    lang_sp=codelang
+    lang_src=codelang
+    lang_dest='vi'
+    if audio_bytes2: #neu co du lieu trong bien audio_bytes2
+        thuam_txt_dichtxt_phatam(audio_bytes2,lang_sp,lang_src,lang_dest)
+
 else:
-    if vaichon==":red[Vietnamse]":
-        lang_sp='vi'
-        lang_src='vi'
-        lang_dest=codelang
-        speech_text_translate_audio(lang_sp,lang_src,lang_dest)
-    else:        
-        lang_sp=codelang
-        lang_src=codelang
-        lang_dest='vi'
-        speech_text_translate_audio(lang_sp,lang_src,lang_dest)
+    pass
 
