24dff2bb24df1b1787336a836aa9ba72b9334a10
tien89talkenvi <147873351+tien89talkenvi@users.noreply.github.com>
2023-11-04 01:13:45 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,23 +14,22 @@ from googletrans import Translator
 from gtts import gTTS, gTTSError   
 from io import BytesIO  
 import base64
-from streamlit.components.v1 import html
 import time
-
+from streamlit.components.v1 import html
 
 def auto_phat_audio(mp3_fp):
     audio_placeholder=st.empty()
     data = mp3_fp.read()
     audio_b64 = base64.b64encode(data).decode()
     my_html=f'''
-            <audio  autoplay>
-                <source src="data:audio/mp3;base64,{audio_b64}">
+            <audio autoplay>
+             <source src="data:audio/mp3;base64,{audio_b64}">
             </audio>
             '''
     audio_placeholder.empty()
     time.sleep(0.2)
-    html(my_html) 
-    #audio_placeholder.markdown(md,unsafe_allow_html=True)
+    #html(my_html) 
+    audio_placeholder.markdown(my_html,unsafe_allow_html=True)
 
 def xuli_ra_phat_am_dest(audio_bytes,lang_sp,lang_src,lang_dest):
     with open('thu.wav','wb') as f:
@@ -53,14 +52,11 @@ def xuli_ra_phat_am_dest(audio_bytes,lang_sp,lang_src,lang_dest):
                 st.write(":blue["+text_translated+"]")
             if text_translated !='':
                 mp3_fp = BytesIO()
-                try:
-                    tts = gTTS(text_translated, lang=lang_dest)
-                    tts.write_to_fp(mp3_fp)
-                    mp3_fp.seek(0)  #phai co dong nay thi auto_phat_audio moi phat dc
-                    st.audio(mp3_fp, format="audio/wav",start_time=0)
-                    auto_phat_audio(mp3_fp)
-                except gTTSError as err:
-                    st.error(err)
+                tts = gTTS(text_translated, lang=lang_dest)
+                tts.write_to_fp(mp3_fp)
+                mp3_fp.seek(0)  #phai co dong nay thi auto_phat_audio moi phat dc
+                st.audio(mp3_fp, format="audio/wav",start_time=0)
+                auto_phat_audio(mp3_fp)
         except sr.UnknownValueError:
             st.write("Không nhận thức được tiếng nói")
         except sr.RequestError as e:
@@ -110,3 +106,4 @@ elif vaichon==":blue[Nói tiếng "+tieng_khac+"]":
 
 else:
     pass
+
