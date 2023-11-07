1e8de6d867a69d6c886f4bc4490178304095b064
tien89talkenvi <147873351+tien89talkenvi@users.noreply.github.com>
2023-11-08 00:15:11 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,4 @@
-#streamlit==1.27.2
-#SpeechRecognition==3.10.0
-#pyaudio==0.2.13 #(cho SpeechRecognition lay micro )
-#googletrans==4.0.0rc1 #(phien ban nay cho rieng py khi su dung googletrans, cac pban khac hay gay loi)
-#gTTS==2.4.0
-
-
 #https://talkenvi-b5vypm7itcecxnkuvne7h9.streamlit.app/ 
-#la url app moi talkenvi
 import streamlit as st
 import speech_recognition as sr 
 from audio_recorder_streamlit import audio_recorder #pip install audio-recorder-streamlit
@@ -16,6 +8,31 @@ from io import BytesIO
 import base64
 import time
 from streamlit.components.v1 import html
+
+def thuc_hien_dao(tieng_1,tieng_2):
+    index_t1=3
+    index_t2=2
+    with col1:
+        tieng_1 = st.selectbox(":red[---NÓI]", 
+                    ("Vietnamese - Viet (vi)","English - Anh (en)","Spanish - Tây ban nha (es)","Taiwan - Đài loan (zh-TW)","Danish - Đan mạch (da)","German - Đức (de)","Dutch - Hà lan (nl)","French - Pháp (fr)","Japanese - Nhật bản (ja)","Korean - Hản quốc (ko)","Thai - Thái lan (th)","Khmer - Campuchia (km)"),index=index_t1,key=1)
+
+    with col2:
+        tieng_2 = st.selectbox(":blue[---NGHE (đã dịch)]", 
+                    ("Vietnamese - Viet (vi)","English - Anh (en)","Spanish - Tây ban nha (es)","Taiwan - Đài loan (zh-TW)","Danish - Đan mạch (da)","German - Đức (de)","Dutch - Hà lan (nl)","French - Pháp (fr)","Japanese - Nhật bản (ja)","Korean - Hản quốc (ko)","Thai - Thái lan (th)","Khmer - Campuchia (km)"),index=index_t2,key=2)
+
+
+def ma_tieng(tieng):
+    global codelang_1,codelang_2
+    sub1='('
+    sub2=')'
+    idx1 = tieng.index(sub1)
+    idx2 = tieng.index(sub2)
+    res = ''
+    # getting elements in between
+    for idx in range(idx1 + len(sub1), idx2):
+        res = res + tieng[idx]
+    codelang=res
+    return codelang
 
 def auto_phat_audio(mp3_fp):
     audio_placeholder=st.empty()
@@ -28,8 +45,8 @@ def auto_phat_audio(mp3_fp):
             '''
     audio_placeholder.empty()
     time.sleep(0.2)
-    #html(my_html) 
-    audio_placeholder.markdown(my_html,unsafe_allow_html=True)
+    html(my_html) 
+    #audio_placeholder.markdown(md,unsafe_allow_html=True)
 
 def xuli_ra_phat_am_dest(audio_bytes,lang_sp,lang_src,lang_dest):
     with open('thu.wav','wb') as f:
@@ -39,24 +56,24 @@ def xuli_ra_phat_am_dest(audio_bytes,lang_sp,lang_src,lang_dest):
         audio = r.record(source)  # read the entire audio file
         try:
             text_from_audio = r.recognize_google(audio, language=lang_sp)
-            if lang_sp=='vi':
+            with col1:
                 st.write(":red["+text_from_audio+"]")
-            else:
-                st.write(":blue["+text_from_audio+"]")
             if text_from_audio != '':
                 translator = Translator()
                 text_translated = translator.translate(text_from_audio, src=lang_src,dest=lang_dest).text    # Dich ra En theo tai lieu web
-            if lang_dest=='vi':
-                st.write(":red["+text_translated+"]")
-            else:
+            with col2:
                 st.write(":blue["+text_translated+"]")
             if text_translated !='':
                 mp3_fp = BytesIO()
-                tts = gTTS(text_translated, lang=lang_dest)
-                tts.write_to_fp(mp3_fp)
-                mp3_fp.seek(0)  #phai co dong nay thi auto_phat_audio moi phat dc
-                st.audio(mp3_fp, format="audio/wav",start_time=0)
-                auto_phat_audio(mp3_fp)
+                try:
+                    tts = gTTS(text_translated, lang=lang_dest)
+                    tts.write_to_fp(mp3_fp)
+                    mp3_fp.seek(0)  #phai co dong nay thi auto_phat_audio moi phat dc
+                    with col2:
+                        st.audio(mp3_fp, format="audio/wav",start_time=0)
+                        auto_phat_audio(mp3_fp)
+                except gTTSError as err:
+                    st.error(err)
         except sr.UnknownValueError:
             st.write("Không nhận thức được tiếng nói")
         except sr.RequestError as e:
@@ -64,46 +81,36 @@ def xuli_ra_phat_am_dest(audio_bytes,lang_sp,lang_src,lang_dest):
             #print(f"Lỗi: {e}")
 
 #######################################################
-st.title(":blue[Trò chuyện có thông dịch bằng tiếng Việt và tiếng...]")
-tieng_khac = st.selectbox(":blue[(Talk with interpretation in Vietnamese and ...)]", 
-                ("English - Anh (en)","Spanish - Tây ban nha (es)","Taiwan - Đài loan (zh-TW)","Danish - Đan mạch (da)","German - Đức (de)","Dutch - Hà lan (nl)","French - Pháp (fr)","Japanese - Nhật bản (ja)","Korean - Hản quốc (ko)","Thai - Thái lan (th)","Khmer - Campuchia (km)"),index=0)
-sub1='('
-sub2=')'
-idx1 = tieng_khac.index(sub1)
-idx2 = tieng_khac.index(sub2)
-res = ''
-# getting elements in between
-for idx in range(idx1 + len(sub1), idx2):
-    res = res + tieng_khac[idx]
-codelang=res
-#print(codelang)
+st.subheader(":blue[Trò chuyện có thông dịch bằng hai ngôn ngữ]")
 
-vaichon = st.radio(":green[Select one of options to say:]", [":red[Nói tiếng VIỆT (Vietnamese)]",":blue[Nói tiếng "+tieng_khac+"]",'CLEAR'],index=2,horizontal=True ) 
+colA, colB,colC = st.columns(3)
+with colA:
+    st.write(':green[Thiết lập hai ngôn ngữ đàm thoại:]')        
+with colB:
+    tieng_1 = st.selectbox(":red[---NÓI]", 
+                ("Vietnamese - Viet (vi)","English - Anh (en)","Spanish - Tây ban nha (es)","Taiwan - Đài loan (zh-TW)","Danish - Đan mạch (da)","German - Đức (de)","Dutch - Hà lan (nl)","French - Pháp (fr)","Japanese - Nhật bản (ja)","Korean - Hản quốc (ko)","Thai - Thái lan (th)","Khmer - Campuchia (km)"),index=0,key=1,label_visibility="hidden" )
+with colC:
+    tieng_2 = st.selectbox(":blue[---NGHE (đã dịch)]", 
+                ("Vietnamese - Viet (vi)","English - Anh (en)","Spanish - Tây ban nha (es)","Taiwan - Đài loan (zh-TW)","Danish - Đan mạch (da)","German - Đức (de)","Dutch - Hà lan (nl)","French - Pháp (fr)","Japanese - Nhật bản (ja)","Korean - Hản quốc (ko)","Thai - Thái lan (th)","Khmer - Campuchia (km)"),index=1,key=2,label_visibility="hidden" )
 
-st.write("---")
+audio_bytes = audio_recorder(text='')
 
-if vaichon==":red[Nói tiếng VIỆT (Vietnamese)]":
-    lang_sp='vi'
-    lang_src='vi'
-    lang_dest=codelang
-    mtext="Click on mic rồi nói tiếng VIỆT (Vietnamese)"
-    # Chay ham hien mic voi cac tham so va thu am roi tra ve audio_bytes thu duoc
-    audio_bytes1 = audio_recorder(text=mtext,recording_color="#FFFF00",neutral_color="#FF0000",icon_size="2x",energy_threshold=(-1.0,1.0),pause_threshold=5.0)
-    if audio_bytes1:
-        # chay ham Xu li audio_bytes da thu de cho ra ket qua cuoi cung la phat ra am thanh dest
-        xuli_ra_phat_am_dest(audio_bytes1,lang_sp,lang_src,lang_dest)
+if audio_bytes:
+    vaichon = st.radio(":green[Chọn một ngôn ngữ dưới đây rồi nhấp micro để NÓI:]", [":red["+tieng_1+"]",":blue["+tieng_2+"]",'CLEAR'],index=2,horizontal=True ) 
 
-elif vaichon==":blue[Nói tiếng "+tieng_khac+"]":
-    lang_sp=codelang
-    lang_src=codelang
-    lang_dest='vi'
-    mtext='Click on mic rồi nói tiếng '+tieng_khac
-    # Chay ham hien mic voi cac tham so va thu am roi tra ve audio_bytes thu duoc
-    audio_bytes2 = audio_recorder(text=mtext,recording_color="#FFFF00",neutral_color="#0000FF",icon_size="2x",energy_threshold=(-1.0,1.0),pause_threshold=5.0)
-    if audio_bytes2:
-        # chay ham Xu li audio_bytes da thu de cho ra ket qua cuoi cung la phat ra am thanh dest
-        xuli_ra_phat_am_dest(audio_bytes2,lang_sp,lang_src,lang_dest)
+    col1, col2 = st.columns(2)
 
-else:
-    pass
-
+    if vaichon==":red["+tieng_1+"]":
+        codelang_1 = ma_tieng(tieng_1)
+        lang_sp=codelang_1
+        lang_src=codelang_1
+        lang_dest=ma_tieng(tieng_2)
+        xuli_ra_phat_am_dest(audio_bytes,lang_sp,lang_src,lang_dest)    
+    elif vaichon==":blue["+tieng_2+"]":
+        codelang_2 = ma_tieng(tieng_2)
+        lang_sp=codelang_2
+        lang_src=codelang_2
+        lang_dest=ma_tieng(tieng_1)
+        xuli_ra_phat_am_dest(audio_bytes,lang_sp,lang_src,lang_dest)    
+    else:
+        audio_bytes=bytes()
